9ed478d40cb976d588041707a10192a3bc2930b4
Holly <71049594+HDuCoing@users.noreply.github.com>
2021-05-29 15:58:41 +1200

    Team Manager App

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace RugbyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.time_And_Date = new System.Windows.Forms.Label();
+            this.load = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.players = new System.Windows.Forms.Button();
+            this.teams = new System.Windows.Forms.Button();
+            this.credit = new System.Windows.Forms.Label();
+            this.myOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.mySaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.time_And_Date);
+            this.panel1.Controls.Add(this.load);
+            this.panel1.Controls.Add(this.save);
+            this.panel1.Controls.Add(this.players);
+            this.panel1.Controls.Add(this.teams);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(161, 692);
+            this.panel1.TabIndex = 0;
+            // 
+            // time_And_Date
+            // 
+            this.time_And_Date.AutoSize = true;
+            this.time_And_Date.Location = new System.Drawing.Point(9, 668);
+            this.time_And_Date.Name = "time_And_Date";
+            this.time_And_Date.Size = new System.Drawing.Size(30, 13);
+            this.time_And_Date.TabIndex = 4;
+            this.time_And_Date.Text = "Time";
+            this.time_And_Date.Click += new System.EventHandler(this.time_And_Date_Click);
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(3, 135);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(107, 31);
+            this.load.TabIndex = 3;
+            this.load.Text = "Load Data Archive";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.loadData_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(3, 97);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(107, 32);
+            this.save.TabIndex = 2;
+            this.save.Text = "Save Data Archive";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.saveData_Click);
+            // 
+            // players
+            // 
+            this.players.Location = new System.Drawing.Point(3, 61);
+            this.players.Name = "players";
+            this.players.Size = new System.Drawing.Size(107, 30);
+            this.players.TabIndex = 1;
+            this.players.Text = "Players";
+            this.players.UseVisualStyleBackColor = true;
+            this.players.Click += new System.EventHandler(this.players_Click);
+            // 
+            // teams
+            // 
+            this.teams.Location = new System.Drawing.Point(3, 24);
+            this.teams.Name = "teams";
+            this.teams.Size = new System.Drawing.Size(107, 31);
+            this.teams.TabIndex = 0;
+            this.teams.Text = "Teams";
+            this.teams.UseVisualStyleBackColor = true;
+            this.teams.Click += new System.EventHandler(this.teams_Click);
+            // 
+            // credit
+            // 
+            this.credit.AutoSize = true;
+            this.credit.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.credit.Location = new System.Drawing.Point(168, 671);
+            this.credit.Name = "credit";
+            this.credit.Size = new System.Drawing.Size(180, 13);
+            this.credit.TabIndex = 4;
+            this.credit.Text = "2021 Rugby League - Holly DuCoing";
+            // 
+            // myOpenFileDialog
+            // 
+            this.myOpenFileDialog.FileName = "myOpenFileDialog";
+            this.myOpenFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.myOpenFileDialogue_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(940, 693);
+            this.Controls.Add(this.credit);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button teams;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button players;
+        private System.Windows.Forms.Label credit;
+        private System.Windows.Forms.Label time_And_Date;
+        private System.Windows.Forms.OpenFileDialog myOpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog mySaveFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RugbyApp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Player
+        {//ID number (unique), name, birth date, height, weight, place of Birth and the team they are signed in.
+            public int ID;
+            public string Name { get; set; }
+            public int DOB { get; set; }
+            public int Height { get; set; }
+            public int Weight { get; set; }
+            public string PlaceOfBirth { get; set; }
+            public string Team { get; set; }
+
+            public Player(int id, string name, int dob, int height, int weight, string placeOfBirth, string team)
+            {
+                ID = id;
+                Name = name;
+                DOB = dob;
+                Height = height;
+                Weight = weight;
+                PlaceOfBirth = placeOfBirth;
+                Team = team;
+                
+            }
+
+
+        }
+        class Teams
+        {// Teams
+            public string Team { get; set; }
+            public string Ground { get; set; }
+            public string Coach { get; set; }
+            public int YearFounded { get; set; }
+            public string Region { get; set; }
+            public string PlayersEnrolled { get; set; }
+            public Teams(string teamName, string coach, string ground, int year_founded, string region, string players_enrolled)
+            {
+                Team = teamName;
+                Ground = ground;
+                Coach = coach;
+                YearFounded = year_founded;
+                Region = region;
+                PlayersEnrolled = players_enrolled;
+            }
+        }
+        private void loadData_Click(object sender, EventArgs e)
+        { // Button
+            List<Player> player_list = new List<Player>();
+            myOpenFileDialog.Title = "Open Text File";
+            myOpenFileDialog.Filter = "TextFile|*.txt";
+            if (myOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (FileStream fStream = File.OpenRead(myOpenFileDialog.FileName))
+                    {
+                        StreamReader reader = new StreamReader(fStream);
+                        string eachPlayer;
+
+                        while ((eachPlayer = reader.ReadLine()) != null)
+                        {
+                            List<string> tokens = new List<string>(eachPlayer.Split(';')); //int id, string name, int dob, int height, int weight, string placeOfBirth, string team
+                            player_list.Add(new Player(Convert.ToInt32(tokens[0]), tokens[1], Convert.ToInt32(tokens[2]), Convert.ToInt32(tokens[3]), Convert.ToInt32(tokens[4]), tokens[5], tokens[6]));
+
+                        }
+                        if (reader != null)
+                        {
+                            reader.Close();
+                        }
+                        if (fStream != null)
+                        {
+                            fStream.Close();
+                        }
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("File not found.");
+                }
+            }
+        }
+        
+
+        private void saveData_Click(object sender, EventArgs e)
+        { // Button
+            
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Title = "Saved Players";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true; //int id, string name, int dob, int height, int weight, string placeOfBirth, string team
+            Player tempPlayer = new Player(0, "", 0, 0, 0, "", "");
+
+            string newPlayer = tempPlayer.ID.ToString() + ";";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    StreamWriter myWriter = new StreamWriter(mySaveFileDialog.FileName, true);
+                    
+                    if (newPlayer.Trim() != "") ;
+                }
+
+                    myStream.Close();
+                }
+            }
+        
+
+        private void players_Click(object sender, EventArgs e)
+        { // Button
+
+        }
+
+        private void teams_Click(object sender, EventArgs e)
+        { // Button
+
+        }
+        // Clock label
+        public static DateTime Now { get; }
+        private void time_And_Date_Click(object sender, EventArgs e)
+        {
+            this.time_And_Date.Text = DateTime.Now.ToString();
+           
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time_And_Date.Text = DateTime.Now.ToString("dd-MMM-yyyy hh:mm:ss tt");
+        }
+
+        private void myOpenFileDialogue_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        // Clock ends
+
+
+    }
+}
